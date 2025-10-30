be90dc8b9a6a837ba0222b6b68a8e1822d27e6ac
Altair <altair.raphael@usp.br>
2025-10-30 00:51:14 -0300

    add solution 1.Two Sum

---
+++ Arrays/Easy/1. Two Sum/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int):
+        size = len(nums)
+        # brute force solution
+        for i in range(size):
+            for j in range(i+1, size):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
