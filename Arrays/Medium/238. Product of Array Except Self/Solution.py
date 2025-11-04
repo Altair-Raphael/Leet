fc98b23a605b0ad05311a18bd668258f37df3730
Altair <altair.raphael@usp.br>
2025-11-04 17:19:46 -0300

    122. Best time to buy stocks II solved

--- Arrays/Medium/238. Product of Array Except Self/Solution.py
+++ Arrays/Medium/238. Product of Array Except Self/Solution.py
@@ -1,22 +1,18 @@
 class Solution:
-    #new solution using pre and post products arrays
+    #new solution using pre and post products arrays and O(1) space complexity by operating inside of the array
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         lenght = len(nums)
         answer = [1]*lenght
-        pre = [1]*lenght
-        post = [1]*lenght
+        pre = 1
+        post = 1
 
-        product = 1
         for i in range(len(nums)):
-            pre[i] = product
-            product = product*nums[i]
+            answer[i] = pre
+            pre = pre*nums[i]
 
         product = 1
         for i in range(len(nums)-1, -1, -1):
-            post[i] = product
-            product = product*nums[i]
-
-        for i in range(len(nums)):
-            answer[i] = pre[i]*post[i]
+            answer[i] = answer[i]*post
+            post = post*nums[i]
 
         return answer
