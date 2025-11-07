fbfd24f6196a1527834f8a0ccba36aced37c29e3
Altair <altairraphaelperez@gmail.com>
2025-11-06 22:26:04 -0300

    optimized 41 first missing positive

--- Arrays/Hard/41. First Missing Positive/Solution.py
+++ Arrays/Hard/41. First Missing Positive/Solution.py
@@ -1,17 +1,24 @@
 class Solution:
+
     #solution using input array as memory
     def firstMissingPositive(self, nums: List[int]) -> int:
+
+        #first iteration gets rid of negative numbers
         for i in range(len(nums)):
             if nums[i]<0:
                 nums[i]=0
-        
-        for i in range (len(nums)):
-            if 1 <= nums[i] <= len(nums):
-                val = abs(nums[i])
 
+        #second iteration marks negative index in the input array to identify what numbers appeared
+        for i in range (len(nums)):
+            val = abs(nums[i])
+            if 1 <= val <= len(nums):
                 if  nums[val -1]>0:
                     nums[val-1] = -1*nums[val-1]
                 elif nums[val-1] == 0:
                     nums[val-1] = -1*(len(nums)+1)
 
-        
+        #third iteration verify the first missing positive
+        for i in range (1, len(nums)+1, +1):
+            if nums[i-1] >= 0:
+                return i
+        return len(nums)+1
