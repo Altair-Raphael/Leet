97712ecdf0d7e3ddbcfb4824d94c951e9e86da7e
Altair <altairraphaelperez@gmail.com>
2025-11-08 08:25:00 -0300

    136. Single number solved using XOR operator

--- Strings/Medium/151. Reverse Words in a String/Solution.py
+++ Strings/Medium/151. Reverse Words in a String/Solution.py
@@ -1,2 +1,7 @@
 class Solution:
+    #solution using string and list methods
     def reverseWords(self, s: str) -> str:
+        words = s.split()
+        words.reverse()
+        wordsfinal = " ".join(words)
+        return wordsfinal
