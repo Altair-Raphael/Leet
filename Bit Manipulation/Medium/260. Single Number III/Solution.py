381f4c1f74aca9e0fed8de6d43729f72b924e885
Altair <altair.raphael@usp.br>
2025-11-19 23:40:49 -0300

    260. Single Number III solved using hashMap

--- Bit Manipulation/Medium/260. Single Number III/Solution.py
+++ Bit Manipulation/Medium/260. Single Number III/Solution.py
@@ -1,3 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         
+        frequencia = {}
+        
+        
+        for num in nums:
+            if num in frequencia :
+                frequencia [num] += 1
+            else:
+                frequencia [num] = 1
+            
+        resposta = []
+        
+        for chave in frequencia:
+            if frequencia[chave] == 1:
+                resposta.append(chave)
+                 
+        return resposta
